e4e14afeb3d16cb386d9d515278ec8eabf1b78fe
chronarchitect <aniketdas674@gmail.com>
2020-06-05 20:58:29 +0530

    Added Image Encryption, Decryption, and homomorphic brightness adjustment on encrypted image.
    also added saving and loading functions for encrypted image objects

---
+++ ImageCryptography.py
@@ -0,0 +1,93 @@
+from PIL import Image
+import numpy as np
+import pickle
+
+import Pailier
+
+def ImgEncrypt(public_key, plainimg):
+    """
+    args:
+        public_key: Pailier PublicKey object
+        plainimg: PIL Image object
+        
+    returns:
+        cipherimg: Encryption of plainimg
+    Encrypts an image
+    """
+    
+    cipherimg = np.asarray(plainimg)
+    shape = cipherimg.shape
+    cipherimg = cipherimg.flatten().tolist()
+    cipherimg = [Pailier.Encrypt(public_key, pix) for pix in cipherimg]
+    
+    return np.asarray(cipherimg).reshape(shape)
+
+
+def ImgDecrypt(public_key, private_key, cipherimg):
+    """
+    args:
+        public_key: Pailier PublicKey object
+        private_key: Pailier PrivateKey object
+        cipherimg: encryption of Image
+        
+    returns:
+        Image object which is the decryption of cipherimage
+    Decrypts ecnrypted image
+    """
+    shape = cipherimg.shape
+    plainimg = cipherimg.flatten().tolist()
+    plainimg = [Pailier.Decrypt(public_key, private_key, pix) for pix in plainimg]
+    plainimg = [pix if pix < 255 else 255 for pix in plainimg]
+    plainimg = [pix if pix > 0 else 0 for pix in plainimg]
+    
+    return Image.fromarray(np.asarray(plainimg).reshape(shape).astype(np.uint8))
+
+
+def homomorphicBrightness(public_key, cipherimg, factor):
+    """
+    args:
+        public_key: Pailier PublicKey object
+        cipherimg: n dimensional array containing encryption of image pixels
+        factor: Amount of brightness to be added (-ve for decreasing brightness)
+    
+    returns:
+        n dimensional array containing encryption of image pixels with brightness adjusted
+    
+    Function to demonstrate homomorphism
+    Performs brightness adjust operation on the encrypted image
+    """
+    shape = cipherimg.shape
+    brightimg = cipherimg.flatten().tolist()
+    brightimg = [Pailier.homomorphic_add_constant(public_key, pix, factor) for pix in brightimg]
+    
+    return np.asarray(brightimg).reshape(shape)
+
+
+def saveEncryptedImg(cipherimg, filename):
+    """
+    args:
+        cipherimg: Encryption of an image
+        filename: filename to save encryption (saved under encrypted-images directory)
+        
+    saves Encryption of image int a file
+    """
+    filename = "encrypted-images/" + filename
+    fstream = open(filename, "wb")
+    pickle.dump(cipherimg, fstream)
+    fstream.close()
+
+
+def loadEncryptedImg(filename):
+    """
+    args:
+        filename: filename of the Encrypted object under encrypted-images directory
+        
+    returns:
+        n-dimensional array containing ecryption of image
+    loads Encrypted image object from file
+    """
+    filename = "encrypted-images/" + filename    
+    fstream = open(filename, "rb")
+    cipherimg = pickle.load(fstream)
+    fstream.close()
+    return cipherimg
